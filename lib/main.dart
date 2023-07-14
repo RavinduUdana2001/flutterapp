33ae6c4f7a17ddc6ac3f1dead308e23361a71342
SRU Singhapura <srusinghapura@students.nsbm.ac.lk>
2023-07-14 10:18:00 +0530

    flutter commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+// ignore: unused_import
+import 'package:flutter_application_1/homepage.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+
+
+
+
+
